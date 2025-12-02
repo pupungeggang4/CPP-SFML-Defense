dea103f543c13be114cf041467a999a46c8c512b
pupungeggang2 <kyongkyong2892@gmail.com>
2025-12-02 13:57:18 +0900

    Started

---
+++ src/include/general.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <memory>
+#include <optional>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+
+using std::shared_ptr;
+using std::make_shared;
+
+#define K_RIGHT 86
+#define K_LEFT 87
+#define K_DOWN 88
+#define K_UP 89
+#define K_ESC 37
+#define K_ENTER 36
+#define K_SPACE 40
